3dc2456dc7cc54777dd2f462c420af83f52ddffa
Mitchkal <mitchellkalenda@gmail.com>
2025-07-13 23:20:22 +0300

    added caching for all properties

--- alx_backend_caching_property_listings/properties/views.py
+++ alx_backend_caching_property_listings/properties/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 from .models import Property
+from .utils import get_all_properties
 from django.http import JsonResponse
 
 @cache_page(60 * 15)
 def property_list(request):
-    properties = Property.Objects.all().values()
+    # properties = Property.Objects.all().values()
+    properties = get_all_properties()
     return JsonResponse({'data': list(properties)}, safe=False)
     # return render(request, 'properties/property_list.html', {'properties': properties})
 
