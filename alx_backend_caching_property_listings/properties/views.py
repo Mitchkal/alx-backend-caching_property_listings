1bff3242be06102092a20770e8c762bbbb64454d
Mitchkal <mitchellkalenda@gmail.com>
2025-07-13 23:07:59 +0300

    added property list view

--- alx_backend_caching_property_listings/properties/views.py
+++ alx_backend_caching_property_listings/properties/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from .models import Property
+
+@cache_page(60 * 15)
+def property_list(request):
+    properties = Property.Objects.all()
+    return render(request, 'properties/property_list.html', {'properties': properties})
 
 # Create your views here.
