d7a5bf21ce2b4715f0126ac907326c0254f802ac
Mitchkal <mitchellkalenda@gmail.com>
2025-07-13 23:14:44 +0300

    added json response

--- alx_backend_caching_property_listings/properties/views.py
+++ alx_backend_caching_property_listings/properties/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 from .models import Property
+from django.http import JsonResponse
 
 @cache_page(60 * 15)
 def property_list(request):
-    properties = Property.Objects.all()
-    return render(request, 'properties/property_list.html', {'properties': properties})
+    properties = Property.Objects.all().values()
+    return JsonResponse({'data': list(properties)}, safe=False)
+    # return render(request, 'properties/property_list.html', {'properties': properties})
 
 # Create your views here.
--- properties/views.py
+++ properties/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+#!/usr/bin/env python3
 from django.views.decorators.cache import cache_page
 from .models import Property
+from django.http import JsonResponse
 
 @cache_page(60 * 15)
 def property_list(request):
     properties = Property.Objects.all()
-    return render(request, 'properties/property_list.html', {'properties': properties})
+    return JsonResponse({'data': list(properties)}, safe=False)
+    # return render(request, 'properties/property_list.html', {'properties': properties})
 
 # Create your views here.
